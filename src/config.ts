177d56de75d7935f5460fbbd069383992cdb24bb
Sevketcan <sevketcan.kalyoncuoglu@std.yeditepe.edu.tr>
2024-12-30 16:58:04 +0300

    Config

--- src/config.ts
+++ src/config.ts
@@ -2,44 +2,19 @@ import { ICollectionType, IConfig } from "./lib/app/types";
 
 const CONFIG: IConfig = {
     coinDenom: "ustars",
-    name: "Embeddable House",
-    chainId: "elgafar-1",
-    createdDate: "2024-03-31T19:01:01.148Z",
-    modifiedDate: "2024-03-31T19:01:01.148Z",
+    name: "TOUR AND TRAVEL",
+    chainId: "galileo-4",
+    createdDate: "2024-12-23T06:44:18.819Z",
+    modifiedDate: "2024-12-23T06:44:18.819Z",
     id: "andromeda",
     collections: [
         {
-            auction:
-                "stars19xddx2zdky6d9jlsrqqyavfycr6pytwmn799vvqsu7fzd4dp7k4q9etd9q",
-            cw721: "stars15l5j0hf80ayj7yuhd6qjhlgwgmgy6apekwpe3k76lgltfhqge3cqemdnth",
-            name: "Auction Example",
-            type: ICollectionType.AUCTION,
-            id: "auction",
-            featured: "ANDR1"
-        },
-        {
-            marketplace:
-                "stars1gmjfd922u5jf8szjwad9k47ylsg0ftg6pvnaa7fplh8vrd0w4z0qa827u0",
-            cw721: "stars1cf0urzkvhph9nhxe5fv8rq25j8x683z3vcjfwc9ypv5qkar0jw2sv78dwu",
-            name: "Marketplace Example",
+            marketplace: "andr1wcpaymvxj4yzxusv6rv0z9nmsk552wuz4mswudd25kr6k9mq2taqqk58yu",
+            cw721: "andr1d5s3y869na6qr4wvsl8ap9e77w6q8sr8d7a7rq48ly2ygetvq8zqhm2ff4",
+            name: "Tour And Travels",
+            id: "embeddables-marketplace-1",
             type: ICollectionType.MARKETPLACE,
-            id: "marketplace",
-        },
-        {
-            crowdfund:
-                "stars1a0el2qceyryftj9cx0ds4as3sgfu4ezj7mumtdp34v5c9qpswzaqw0qq38",
-            cw721: "stars1uj5tl3g26022ev9ym6x39xtv09hz3xvc7e3c5wthdpufxtwk7rsscs6jz4",
-            name: "Crowdfund Example",
-            type: ICollectionType.CROWDFUND,
-            id: "crowdfund",
-        },
-        {
-            exchange:
-                "stars1a9l4h3dkh6athk9z2qax63reg4ft5zs32sqdze6ruhdhxsdk8u6s4mpxyy",
-            cw20: "stars1ud5j93qj24zae3dgdugsqjkk9ez6hmg23c8gku4zygmvewn39x8sfmrw4k",
-            name: "Exchange Example",
-            type: ICollectionType.EXCHANGE,
-            id: "exchange",
+            featured: "HTL"
         },
     ],
 };
